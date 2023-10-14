2a9acd90906d4fa8a0bb3c901de3ebe2a581a3c5
Alvin Kong <alvin.kong@ha.org.hk>
2023-10-14 23:29:32 +0800

    13/10
    new

--- my-app/src/component/Nav.js
+++ my-app/src/component/Nav.js
@@ -1,38 +1,49 @@
 import logo from '../image/Logo.svg'
 import Container from 'react-bootstrap/Container';
+import { useState } from 'react';
 
 
 
 function Nav() {
+    const [menuOpen,setMenuOpen] = useState(false);
+    const toggleMenu = () =>{
+        setMenuOpen(!menuOpen);
+    };
     return (
         <nav>
-            <a herf='/'>
-                <img src={logo} alt='logo' />
-            </a>
-{/*             <div>
-                <div className='bar'></div>
-                <div className='bar'></div>
-                <div className='bar'></div>
-            </div> */}
             <ul className='direactBar'>
-                <li>
-                    <a herf="#">Home</a>
+                <li className='Nav-hover-pointer-effect'>
+                    <a herf='/'>
+                        <img src={logo} alt='logo' />
+                    </a>
                 </li>
-                <li>
-                    <a herf="#">About</a>
-                </li>
-                <li>
-                    <a herf="#">Menu</a>
-                </li>
-                <li>
-                    <a herf="#">Reservations</a>
-                </li>
-                <li>
-                    <a herf="#">Order Online</a>
-                </li>
-                <li>
-                    <a herf="#">Login</a>
-                </li>
+                <div className='hambergerIcon Nav-hover-pointer-effect'>
+                    <div className='bar'></div>
+                    <div className='bar'></div>
+                    <div className='bar'></div>
+                </div>
+                <ul className='nav-link1'>
+                {/* <ul className='navLink'> */}
+                    <li className='Nav-hover-effect'>
+                        <a herf="#">Home</a>
+                    </li>
+                    <li className='Nav-hover-effect'>
+                        <a herf="#">About</a>
+                    </li>
+                    <li className='Nav-hover-effect'>
+                        <a herf="#">Menu</a>
+                    </li>
+                    <li className='Nav-hover-effect'>
+                        <a herf="#">Reservations</a>
+                    </li>
+                    <li className='Nav-hover-effect'>
+                        <a herf="#">Order Online</a>
+                    </li>
+                    <li className='Nav-hover-effect'>
+                        <a herf="#">Login</a>
+                    </li>
+                </ul>
+
             </ul>
         </nav>
     );
